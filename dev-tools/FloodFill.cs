76ae8893a521504f3f2643394c1da3f7e06aa307
Geoyeob Kim <gasbank@msn.com>
2019-07-12 21:47:20 +0900

    컬러 데이터 추출 프로그램 작성 중~

---
+++ dev-tools/FloodFill.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace black_dev_tools {
+    partial class FloodFill {
+        static bool ColorMatch(Rgba32 a, Rgba32 b) {
+            return a == b;
+        }
+
+        static Rgba32 GetPixel(Image<Rgba32> bitmap, int x, int y) {
+            return bitmap[x, y];
+        }
+
+        static void SetPixel(Image<Rgba32> bitmap, int x, int y, Rgba32 c) {
+            bitmap[x, y] = c;
+        }
+
+        public static Vector2Int Execute(Image<Rgba32> bitmap, Vector2Int pt, Rgba32 targetColor, Rgba32 replacementColor, out int pixelArea) {
+            Queue<Vector2Int> q = new Queue<Vector2Int>();
+            q.Enqueue(pt);
+            var fillMinPoint = new Vector2Int(bitmap.Width, bitmap.Height);
+            pixelArea = 0;
+            while (q.Count > 0) {
+                var n = q.Dequeue();
+                if (!ColorMatch(GetPixel(bitmap, n.x, n.y), targetColor))
+                    continue;
+                Vector2Int w = n, e = new Vector2Int(n.x + 1, n.y);
+                while ((w.x >= 0) && ColorMatch(GetPixel(bitmap, w.x, w.y), targetColor)) {
+                    SetPixel(bitmap, w.x, w.y, replacementColor);
+                    UpdateFillMinPoint(ref fillMinPoint, w);
+                    pixelArea++;
+                    if ((w.y > 0) && ColorMatch(GetPixel(bitmap, w.x, w.y - 1), targetColor))
+                        q.Enqueue(new Vector2Int(w.x, w.y - 1));
+                    if ((w.y < bitmap.Height - 1) && ColorMatch(GetPixel(bitmap, w.x, w.y + 1), targetColor))
+                        q.Enqueue(new Vector2Int(w.x, w.y + 1));
+                    w.x--;
+                }
+                while ((e.x <= bitmap.Width - 1) && ColorMatch(GetPixel(bitmap, e.x, e.y), targetColor)) {
+                    SetPixel(bitmap, e.x, e.y, replacementColor);
+                    UpdateFillMinPoint(ref fillMinPoint, e);
+                    pixelArea++;
+                    if ((e.y > 0) && ColorMatch(GetPixel(bitmap, e.x, e.y - 1), targetColor))
+                        q.Enqueue(new Vector2Int(e.x, e.y - 1));
+                    if ((e.y < bitmap.Height - 1) && ColorMatch(GetPixel(bitmap, e.x, e.y + 1), targetColor))
+                        q.Enqueue(new Vector2Int(e.x, e.y + 1));
+                    e.x++;
+                }
+            }
+            return fillMinPoint;
+        }
+
+        private static void UpdateFillMinPoint(ref Vector2Int fillMinPoint, Vector2Int w) {
+            if (fillMinPoint.x > w.x || (fillMinPoint.x == w.x && fillMinPoint.y > w.y)) {
+                fillMinPoint = w;
+            }
+        }
+    }
+}
---
+++ dev-tools/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System.Linq;
+
+namespace black_dev_tools {
+    class Program {
+        static void Main(string[] args) {
+            using (Image<Rgba32> image = Image.Load("/Users/kimgeoyeob/black/Art/190527_Colored.png")) {
+                var whiteCount = 0;
+                Dictionary<Rgba32, int> pixelCountByColor = new Dictionary<Rgba32, int>();
+                Dictionary<Vector2Int, Rgba32> islandColorByMinPoint = new Dictionary<Vector2Int, Rgba32>();
+                Dictionary<Vector2Int, int> islandPixelAreaByMinPoint = new Dictionary<Vector2Int, int>();
+                Dictionary<Rgba32, int> islandCountByColor = new Dictionary<Rgba32, int>();
+                Dictionary<int, int> islandCountByPixelArea = new Dictionary<int, int>();
+
+                for (int h = 0; h < image.Height; h++) {
+                    for (int w = 0; w < image.Width; w++) {
+                        var pixelColor = image[w, h];
+                        if (pixelColor == Rgba32.White) {
+                            whiteCount++;
+                        }
+                        IncreaseCountOfDictionaryValue(pixelCountByColor, pixelColor);
+
+                        if (pixelColor == Rgba32.Black) {
+
+                        } else {
+                            var fillMinPoint = FloodFill.Execute(image, new Vector2Int(w, h), pixelColor, Rgba32.Black, out var pixelArea);
+                            if (fillMinPoint != new Vector2Int(image.Width, image.Height)) {
+                                islandColorByMinPoint[fillMinPoint] = pixelColor;
+                                islandPixelAreaByMinPoint[fillMinPoint] = pixelArea;
+                                IncreaseCountOfDictionaryValue(islandCountByPixelArea, pixelArea);
+                                IncreaseCountOfDictionaryValue(islandCountByColor, pixelColor);
+                            } else {
+                                throw new Exception("Invalid fill min point!");
+                            }
+                        }
+                    }
+                }
+                image.Save("bar.jpg"); // Automatic encoder selected based on extension.
+                Console.WriteLine($"Total Pixel Count: {image.Width * image.Height}");
+                Console.WriteLine($"White Count: {whiteCount}");
+
+                var islandIndex = 0;
+                foreach (var kv in islandColorByMinPoint) {
+                    Console.WriteLine($"Island #{islandIndex} fillMinPoint={kv.Key}, color={kv.Value}, area={islandPixelAreaByMinPoint[kv.Key]}");
+                    islandIndex++;
+                }
+
+                var colorCountIndex = 0;
+                foreach (var kv in pixelCountByColor) {
+                    islandCountByColor.TryGetValue(kv.Key, out var islandCount);
+                    Console.WriteLine($"Color #{colorCountIndex} {kv.Key}: pixelCount={kv.Value}, islandCount={islandCount}");
+                    colorCountIndex++;
+                }
+
+                var pixelAreaCountIndex = 0;
+                foreach (var kv in islandCountByPixelArea.OrderByDescending(kv => kv.Key)) {
+                    Console.WriteLine($"Pixel Area #{pixelAreaCountIndex} {kv.Key}: islandCount={kv.Value}");
+                    pixelAreaCountIndex++;
+                }
+            }
+        }
+
+        private static void IncreaseCountOfDictionaryValue<T>(Dictionary<T, int> pixelCountByColor, T pixel) {
+            pixelCountByColor.TryGetValue(pixel, out var currentCount);
+            pixelCountByColor[pixel] = currentCount + 1;
+        }
+    }
+}
