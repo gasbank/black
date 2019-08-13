17fd86c4f91a6ea7f752c9a36a891757c69b1b95
Geoyeob Kim <gasbank@msn.com>
2019-08-13 13:25:37 +0900

    개발 편의를 위해서 섬 레이블링 오브젝트 그룹화

--- Assets/Scripts/IslandLabelSpawner.cs
+++ Assets/Scripts/IslandLabelSpawner.cs
@@ -28,20 +28,32 @@ public class IslandLabelSpawner : MonoBehaviour {
 
     public void CreateAllLabels(StageData stageData) {
         var maxRectDict = stageData.islandDataByMinPoint.ToDictionary(e => e.Key, e => GetRectRange(e.Value.maxRect));
-        var area = maxRectDict.Select(e => e.Value.size.x * e.Value.size.y).OrderByDescending(e => e).Take(20).LastOrDefault();
+        //var area = maxRectDict.Select(e => e.Value.size.x * e.Value.size.y).OrderByDescending(e => e).Take(20).LastOrDefault();
+        var rectIndex = 0;
+        var subgroupCapacity = 50;
+        GameObject islandLabelNumberSubgroup = null;
         foreach (var kv in maxRectDict) {
-            //if (kv.Value.size.x * kv.Value.size.y >= area) {
-                //Debug.Log($"Big sub rect island: ({kv.Value.xMin},{kv.Value.yMin})-({kv.Value.xMax},{kv.Value.yMax}) area={kv.Value.size.x * kv.Value.size.y}");
-                var label = Instantiate(islandLabelNumberPrefab, islandLabelNumberGroup);
-                var labelRt = label.GetComponent<RectTransform>();
-                var texSizeFloat = (float)gridWorld.texSize;
-                labelRt.anchoredPosition = new Vector2(kv.Value.center.x / texSizeFloat * rt.sizeDelta.x - rt.sizeDelta.x / 2, kv.Value.center.y / texSizeFloat * rt.sizeDelta.y - rt.sizeDelta.y / 2);
-                labelRt.sizeDelta = new Vector2(kv.Value.size.x / texSizeFloat * rt.sizeDelta.x, kv.Value.size.y / texSizeFloat * rt.sizeDelta.y);
-                var labelText = label.GetComponent<TMPro.TextMeshProUGUI>();
-                var paletteIndex = paletteButtonGroup.GetPaletteIndexByColor(stageData.islandDataByMinPoint[kv.Key].rgba);
-                labelText.text = (paletteIndex + 1).ToString();
-                labelByMinPoint[kv.Key] = label;
-            //}
+            //Debug.Log($"Big sub rect island: ({kv.Value.xMin},{kv.Value.yMin})-({kv.Value.xMax},{kv.Value.yMax}) area={kv.Value.size.x * kv.Value.size.y}");
+            if (rectIndex % subgroupCapacity == 0) {
+                islandLabelNumberSubgroup = new GameObject($"Island Label Subgroup ({rectIndex:d4}-{rectIndex+subgroupCapacity-1:d4})");
+                islandLabelNumberSubgroup.transform.parent = islandLabelNumberGroup;
+                var subGroupRt = islandLabelNumberSubgroup.AddComponent<RectTransform>();
+                subGroupRt.anchoredPosition3D = Vector3.zero;
+                subGroupRt.localScale = Vector3.one;
+            }
+            var label = Instantiate(islandLabelNumberPrefab, islandLabelNumberSubgroup.transform);
+            var labelRt = label.GetComponent<RectTransform>();
+            var texSizeFloat = (float)gridWorld.texSize;
+            var anchoredPosition = kv.Value.center / texSizeFloat * rt.sizeDelta - rt.sizeDelta / 2;
+            labelRt.anchoredPosition = anchoredPosition;
+            var sizeDelta = (Vector2)kv.Value.size / texSizeFloat * rt.sizeDelta;
+            labelRt.sizeDelta = sizeDelta;
+            var labelText = label.GetComponent<TMPro.TextMeshProUGUI>();
+            var paletteIndex = paletteButtonGroup.GetPaletteIndexByColor(stageData.islandDataByMinPoint[kv.Key].rgba);
+            labelText.text = (paletteIndex + 1).ToString();
+            labelByMinPoint[kv.Key] = label;
+            label.name = $"Island Label {rectIndex:d4} #{paletteIndex + 1:d2}";
+            rectIndex++;
         }
     }
 
