007819d4625b7db72d8a998d14891a0f70c321cb
Geoyeob Kim <gasbank@msn.com>
2019-08-13 15:18:52 +0900

    섬 텍스트가 중앙에 오는지 확인하기 위해 디버그 용도의 사각형 렌더링

---
+++ Assets/Scripts/IslandLabel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IslandLabel : MonoBehaviour {
+    [SerializeField] Image backgroundImage = null;
+    [SerializeField] TMPro.TextMeshProUGUI text = null;
+    [SerializeField] RectTransform rt = null;
+
+    public RectTransform Rt => rt;
+
+    public string Text {
+        get => text.text;
+        set => text.text = value;
+    }
+
+    void OnValidate() {
+        rt = GetComponent<RectTransform>();
+    }
+}
--- Assets/Scripts/IslandLabelSpawner.cs
+++ Assets/Scripts/IslandLabelSpawner.cs
@@ -10,7 +10,7 @@ public class IslandLabelSpawner : MonoBehaviour {
     [SerializeField] PaletteButtonGroup paletteButtonGroup = null;
     [SerializeField] Transform islandLabelNumberGroup = null;
 
-    Dictionary<uint, GameObject> labelByMinPoint = new Dictionary<uint, GameObject>();
+    Dictionary<uint, IslandLabel> labelByMinPoint = new Dictionary<uint, IslandLabel>();
 
     void OnValidate() {
         rt = GetComponent<RectTransform>();
@@ -18,9 +18,9 @@ public class IslandLabelSpawner : MonoBehaviour {
 
     RectInt GetRectRange(ulong maxRectUlong) {
         var xMin = (int)(maxRectUlong & 0xffff);
-        var yMax = gridWorld.texSize - (int)((maxRectUlong >> 16) & 0xffff) - 1;
+        var yMax = gridWorld.texSize - (int)((maxRectUlong >> 16) & 0xffff);
         var xMax = (int)((maxRectUlong >> 32) & 0xffff);
-        var yMin = gridWorld.texSize - (int)((maxRectUlong >> 48) & 0xffff) - 1;
+        var yMin = gridWorld.texSize - (int)((maxRectUlong >> 48) & 0xffff);
         var r = new RectInt(xMin, yMin, xMax - xMin, yMax - yMin);
         //Debug.Log($"{r.xMin},{r.yMin} -- {r.xMax},{r.yMax} (area={r.size.x * r.size.y})");
         return r;
@@ -41,16 +41,15 @@ public class IslandLabelSpawner : MonoBehaviour {
                 subGroupRt.anchoredPosition3D = Vector3.zero;
                 subGroupRt.localScale = Vector3.one;
             }
-            var label = Instantiate(islandLabelNumberPrefab, islandLabelNumberSubgroup.transform);
-            var labelRt = label.GetComponent<RectTransform>();
+            var label = Instantiate(islandLabelNumberPrefab, islandLabelNumberSubgroup.transform).GetComponent<IslandLabel>();
+            var labelRt = label.Rt;
             var texSizeFloat = (float)gridWorld.texSize;
             var anchoredPosition = kv.Value.center / texSizeFloat * rt.sizeDelta - rt.sizeDelta / 2;
             labelRt.anchoredPosition = anchoredPosition;
             var sizeDelta = (Vector2)kv.Value.size / texSizeFloat * rt.sizeDelta;
             labelRt.sizeDelta = sizeDelta;
-            var labelText = label.GetComponent<TMPro.TextMeshProUGUI>();
             var paletteIndex = paletteButtonGroup.GetPaletteIndexByColor(stageData.islandDataByMinPoint[kv.Key].rgba);
-            labelText.text = (paletteIndex + 1).ToString();
+            label.Text = (paletteIndex + 1).ToString();
             labelByMinPoint[kv.Key] = label;
             label.name = $"Island Label {rectIndex:d4} #{paletteIndex + 1:d2}";
             rectIndex++;
@@ -59,7 +58,7 @@ public class IslandLabelSpawner : MonoBehaviour {
 
     public void DestroyLabelByMinPoint(uint minPointUint) {
         if (labelByMinPoint.TryGetValue(minPointUint, out var label)) {
-            Destroy(label);
+            Destroy(label.gameObject);
             labelByMinPoint.Remove(minPointUint);
         } else {
             Debug.LogError($"DestroyLabelByMinPoint: could not find minPointUint {minPointUint}!");
