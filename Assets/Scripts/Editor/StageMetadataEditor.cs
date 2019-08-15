b69a6dc26b2313ff896ded93284da6691734af86
Geoyeob Kim <gasbank@msn.com>
2019-08-15 23:53:20 +0900

    파일 정리 중

---
+++ Assets/Scripts/Editor/StageMetadataEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class StageMetadataEditor {
+    [MenuItem("Assets/Create/Stage Metadata")]
+    public static void CreateAssetMenu() {
+        var asset = ScriptableObject.CreateInstance<StageMetadata>();
+        AssetDatabase.CreateAsset(asset, "Assets/Resources/StageMetadata.asset");
+		EditorUtility.SetDirty(asset);
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = asset;
+    }
+}
---
+++ Assets/Scripts/StageMetadata.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class StageMetadata : ScriptableObject {
+    [SerializeField] Sprite gridWorldSprite = null;
+    [SerializeField] Material skipBlackMaterial = null;
+    [SerializeField] Sprite sdfSprite = null;
+    [SerializeField] Material sdfMaterial = null;
+    [SerializeField] TextAsset islandData = null;
+}
