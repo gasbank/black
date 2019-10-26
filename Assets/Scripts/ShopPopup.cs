0ca2f3e0f225fc60e3e0f2098dec96d88435ff11
Geoyeob Kim <gasbank@gmail.com>
2019-10-26 19:21:31 +0900

    크아아아아

---
+++ Assets/Scripts/ShopPopup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ShopPopup : MonoBehaviour {
+    [SerializeField] GameObject shopPropEntry = null;
+    [SerializeField] Transform miniroom = null;
+    [SerializeField] Transform shopPropEntryParent = null;
+
+    private void AddAllProps() {
+        var transformList = miniroom.GetComponentsInChildren<Transform>(true);
+        foreach (var t in transformList) {
+            var entry = Instantiate(shopPropEntry, shopPropEntryParent).GetComponent<ShopPropEntry>();
+            entry.PropName = t.name;
+            entry.PropTarget = t.gameObject;
+        }
+    }
+
+    private void Awake() {
+        AddAllProps();
+    }
+}
---
+++ Assets/Scripts/ShopPropEntry.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopPropEntry : MonoBehaviour {
+    [SerializeField] Text propName = null;
+    [SerializeField] Text propPrice = null;
+    [SerializeField] GameObject propTarget = null;
+
+    public GameObject PropTarget { get => propTarget; set => propTarget = value; }
+    public bool PropTargetActive { get => propTarget.activeSelf; set => propTarget.SetActive(value); }
+    public string PropName { get => propName.text; set => propName.text = value; }
+    public string PropPrice { get => propPrice.text; set => propPrice.text = value; }
+}
