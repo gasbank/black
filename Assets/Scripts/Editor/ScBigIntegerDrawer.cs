39997582f77972cc8462a78bf9eaa2f398cf7ba3
Geoyeob Kim <gasbank@gmail.com>
2019-11-11 14:30:16 +0900

    저장을 제대로 합시다~

---
+++ Assets/Scripts/Editor/ScBigIntegerDrawer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ScBigInteger))]
+public class ScBigIntegerDrawer : PropertyDrawer {
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        EditorGUI.BeginProperty(position, label, property);
+
+        // Draw label
+        position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+        // Don't make child fields be indented
+        var indent = EditorGUI.indentLevel;
+        EditorGUI.indentLevel = 0;
+
+        // Calculate rects
+        var valueRect = new Rect(position.x, position.y, position.width, position.height);
+        var scBigInteger = (ScBigInteger)fieldInfo.GetValue(property.serializedObject.targetObject);
+        var newScBigIntegerStr = EditorGUI.TextField(valueRect, scBigInteger);
+        fieldInfo.SetValue(property.serializedObject.targetObject, new ScBigInteger(newScBigIntegerStr));
+
+        // Set indent back to what it was
+        EditorGUI.indentLevel = indent;
+
+        EditorGUI.EndProperty();
+    }
+}
