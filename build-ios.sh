fd2b13b31b1ca5e358aaffea0ae59d3f59288426
Geoyeob Kim <gasbank@msn.com>
2019-11-19 03:46:30 +0900

    iOS도 2019.2.12f1로

--- build-ios.sh
+++ build-ios.sh
@@ -4,7 +4,7 @@ if [ -z "${BUILD_NUMBER}" ]; then
   BUILD_NUMBER="<NO ENV>"
 fi
 
-/Applications/Unity/Hub/Editor/2019.2.11f1/Unity.app/Contents/MacOS/Unity \
+/Applications/Unity/Hub/Editor/2019.2.12f1/Unity.app/Contents/MacOS/Unity \
     -quit \
     -batchmode \
     -executeMethod BlackBuild.PerformIosBuild \
