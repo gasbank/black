ae9e86bc181dc2f8f16567c6f573f38d61195f3e
Geoyeob Kim <gasbank@gmail.com>
2020-01-05 13:49:59 +0900

    입력 이미지 자동화 작업중

--- dev-tools/SDFTextureGenerator.cs
+++ dev-tools/SDFTextureGenerator.cs
@@ -147,7 +147,7 @@ namespace black_dev_tools {
 			if (maxInside > 0f) {
 				for (y = 0; y < height; y++) {
 					for (x = 0; x < width; x++) {
-						pixels[x, y].alpha = 1f - source.GetPixel(x, y).A/255f;
+						pixels[x, y].alpha = 1f - source.GetPixel(x, y).A / 255f;
 					}
 				}
 				ComputeEdgeGradients();
