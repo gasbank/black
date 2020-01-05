9290deccd0d3a2ad077c3b8b521d6954cafa3e4f
Geoyeob Kim <gasbank@gmail.com>
2020-01-05 15:51:10 +0900

    SDF 자동화 완료, 스테이지 데이터 파일 정리

--- dev-tools/Program.cs
+++ dev-tools/Program.cs
@@ -48,7 +48,7 @@ namespace black_dev_tools {
                 outputPathReplaceTo = args[3];
             }
 
-            var bbFileName = ProcessBoxBlur(sourceFileName, 2);
+            var bbFileName = ExecuteBoxBlur(sourceFileName, 2);
             return ExecuteSdf(bbFileName);
         }
 
@@ -86,7 +86,7 @@ namespace black_dev_tools {
             return avg;
         }
 
-        private static string ProcessBoxBlur(string sourceFileName, int radius) {
+        private static string ExecuteBoxBlur(string sourceFileName, int radius) {
             var targetFileName = AppendToFileName(sourceFileName, "-BB");
             using (var image = Image.Load<Rgba32>(sourceFileName)) {
                 
@@ -189,6 +189,8 @@ namespace black_dev_tools {
                 var fotsFileName = ExecuteFlattenedOutlineToSource(qFileName, fsnbFileName);
                 var bytesFileName = ExecuteDetermineIsland(fotsFileName, startFileName);
                 ExecuteDetermineIslandTest(fsnbFileName, bytesFileName);
+                var bbFileName = ExecuteBoxBlur(fsnbFileName, 2);
+                ExecuteSdf(bbFileName);
             } else {
                 Console.Out.WriteLine($"Unknown mode provided: {mode}");
                 return;
@@ -504,255 +506,6 @@ namespace black_dev_tools {
                 }
             }
             return targetFileName;
-        }
-
-        private static void ExecuteAll(ref string sourcePngFileName, string stageName) {
-            var outputDir = Path.Combine("..", "Assets", "Stages", stageName);
-            Directory.CreateDirectory(outputDir);
-
-            var quantizedFileName = Quantize(sourcePngFileName, stageName, outputDir, 3);
-
-            // 히익...
-            sourcePngFileName = quantizedFileName;
-
-            // 완벽한 검은색은 Outline, 그 이외의 모든 색은 흰색으로 칠해진
-            // 파일을 만든다.
-            WriteOutlineImageFile(sourcePngFileName, stageName, outputDir);
-
-            // 이미지 파일을 열어봅시다~
-            using (var image = Image.Load<Rgba32>(sourcePngFileName)) {
-                // 색상 별 픽셀 수
-                Dictionary<Rgba32, int> pixelCountByColor = new Dictionary<Rgba32, int>();
-                // Min Point 별 (섬 별) 섬 색상
-                Dictionary<Vector2Int, Rgba32> islandColorByMinPoint = new Dictionary<Vector2Int, Rgba32>();
-                // Min Point 별 (섬 별) 섬 픽셀 수(면적)
-                Dictionary<Vector2Int, int> islandPixelAreaByMinPoint = new Dictionary<Vector2Int, int>();
-                // 색상 별 섬 수
-                Dictionary<Rgba32, int> islandCountByColor = new Dictionary<Rgba32, int>();
-                // 픽셀 수(면적) 별 섬 수
-                Dictionary<int, int> islandCountByPixelArea = new Dictionary<int, int>();
-                // Min Point 별 (섬 별) Max Rect
-                Dictionary<uint, ulong> maxRectByMinPoint = new Dictionary<uint, ulong>();
-
-                // 각 픽셀에 대해서 반복한다.
-                for (int h = 0; h < image.Height; h++) {
-                    for (int w = 0; w < image.Width; w++) {
-                        var pixelColor = image[w, h];
-
-                        if (pixelColor == Rgba32.Black) {
-                            // 경계선 색상(검은색)이면 할 일이 없다.
-                        } else {
-                            // (w, h) 좌표부터 검은색이 아닌 색을 검은색으로 채우면서 픽셀 수집한다.
-                            // 수집한 모든 픽셀은 points에, points의 min point는 반환값으로 받는다.
-                            var coord = new Vector2Int(w, h);
-                            var fillMinPoint = FloodFill.ExecuteFillIfNotBlack(image, coord, Rgba32.Black, out var pixelArea, out var points, out var originalColors);
-                            if (fillMinPoint != new Vector2Int(image.Width, image.Height)) {
-                                if (originalColors.Count > 1) {
-                                    // 한 섬에 색상이 여러 가지라면 가장 많은 색상이 최종 색깔이 되도록 하자.
-                                    // 주로 경계선 주변에서 경계선과 섬 색깔이 블렌딩되면서 다른 색깔이 되는 패턴이다.
-                                    //var prominentColor = originalColors.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-                                    var prominentColor = originalColors.OrderByDescending(e => e.Value).Where(e => e.Key != Rgba32.White).First().Key;
-
-                                    pixelColor = prominentColor;
-
-                                    // foreach (var originalColor in originalColors) {
-                                    //     Console.WriteLine($"{originalColor.Key} = {originalColor.Value}");
-                                    // }
-                                    // throw new Exception($"Island color is not uniform! It has {originalColors.Count} colors in it! coord={coord}");
-                                }
-                                if (originalColors.Count == 0) {
-                                    throw new Exception($"Island color is empty. Is this possible?");
-                                }
-
-                                if (pixelColor == Rgba32.White) {
-                                    throw new Exception($"Island color is WHITE?! Fix it!");
-                                }
-
-                                IncreaseCountOfDictionaryValue(pixelCountByColor, pixelColor);
-
-                                islandColorByMinPoint[fillMinPoint] = pixelColor;
-                                islandPixelAreaByMinPoint[fillMinPoint] = pixelArea;
-                                IncreaseCountOfDictionaryValue(islandCountByPixelArea, pixelArea);
-                                IncreaseCountOfDictionaryValue(islandCountByColor, pixelColor);
-                                var xMax = points.Max(e => e.x);
-                                var xMin = points.Min(e => e.x);
-                                var yMax = points.Max(e => e.y);
-                                var yMin = points.Min(e => e.y);
-                                var subRectW = xMax - xMin + 1;
-                                var subRectH = yMax - yMin + 1;
-                                var A = Enumerable.Range(0, subRectH).Select(e => new int[subRectW]).ToArray();
-                                foreach (var point in points) {
-                                    A[point.y - yMin][point.x - xMin] = 1;
-                                }
-                                var area = MaxSubRect.MaxRectangle(subRectH, subRectW, A, out var beginIndexR, out var endIndexR, out var beginIndexC, out var endIndexC);
-                                Console.WriteLine($"Sub Rect: area:{area} [({yMin + beginIndexR},{xMin + beginIndexC})-({yMin + endIndexR},{xMin + endIndexC})]");
-                                maxRectByMinPoint[Vector2IntToUInt32(fillMinPoint)] = GetRectRange(
-                                    xMin + beginIndexC,
-                                    yMin + beginIndexR,
-                                    xMin + endIndexC,
-                                    yMin + endIndexR);
-                            } else {
-                                throw new Exception("Invalid fill min point!");
-                            }
-                        }
-                    }
-                }
-
-                Console.WriteLine($"Total Pixel Count: {image.Width * image.Height}");
-                pixelCountByColor.TryGetValue(Rgba32.White, out var whiteCount);
-                Console.WriteLine($"White Count: {whiteCount}");
-
-                var islandIndex = 0;
-                foreach (var kv in islandColorByMinPoint) {
-                    Console.WriteLine($"Island #{islandIndex} fillMinPoint={kv.Key}, color={kv.Value}, area={islandPixelAreaByMinPoint[kv.Key]}");
-                    islandIndex++;
-                }
-
-                var colorCountIndex = 0;
-                foreach (var kv in pixelCountByColor) {
-                    islandCountByColor.TryGetValue(kv.Key, out var islandCount);
-                    Console.WriteLine($"Color #{colorCountIndex} {kv.Key}: pixelCount={kv.Value}, islandCount={islandCount}");
-                    colorCountIndex++;
-                    if (kv.Key == Rgba32.White) {
-                        throw new Exception("Palette color should not be white!");
-                    }
-                }
-
-                var pixelAreaCountIndex = 0;
-                foreach (var kv in islandCountByPixelArea.OrderByDescending(kv => kv.Key)) {
-                    Console.WriteLine($"Pixel Area #{pixelAreaCountIndex} {kv.Key}: islandCount={kv.Value}");
-                    pixelAreaCountIndex++;
-                }
-
-                Dictionary<uint, uint> islandColorByMinPointPrimitive = new Dictionary<uint, uint>();
-                foreach (var kv in islandColorByMinPoint) {
-                    var p = Vector2IntToUInt32(kv.Key);
-                    var c = Rgba32ToUInt32(kv.Value);
-                    islandColorByMinPointPrimitive[p] = c;
-                }
-
-                // 너무 작은 섬이 있다면 원본 파일(sourcePngFileName)에다가
-                // 작은 섬을 제거한(검은색으로 칠한) 상태를 만들어 원본 위치와 같은 곳에 쓴다.
-                // 그리고 프로그램 종료시킨다.
-                bool smallIslandRemoved = false;
-                using (var outlineImage = Image.Load<Rgba32>(sourcePngFileName)) {
-
-                    foreach (var island in islandPixelAreaByMinPoint) {
-                        if (island.Value < 4 * 4) {
-
-                            var fillMinPoint = FloodFill.ExecuteFillIfNotBlack(outlineImage, island.Key, Rgba32.Black, out var pixelArea, out var points, out var originalColors);
-                            if (fillMinPoint != new Vector2Int(outlineImage.Width, outlineImage.Height) && pixelArea == island.Value) {
-                                smallIslandRemoved = true;
-                            } else {
-                                Console.WriteLine("Logic Error!");
-                            }
-                        }
-                    }
-
-                    if (smallIslandRemoved) {
-                        // 원래 파일에 덮어 쓴다!
-                        using (var stream = new FileStream(sourcePngFileName + "-SmallIslandRemoved.png", FileMode.Create)) {
-                            outlineImage.SaveAsPng(stream);
-                            stream.Close();
-                        }
-
-                        Console.WriteLine("TOO SMALL ISLANDS DETECTED. Small Island Removed file created.");
-                        Console.WriteLine("Abort...");
-                        return;
-                    }
-                }
-
-                var stageData = new StageData();
-                foreach (var kv in islandPixelAreaByMinPoint) {
-                    var p = Vector2IntToUInt32(kv.Key);
-                    stageData.islandDataByMinPoint[p] = new IslandData {
-                        pixelArea = islandPixelAreaByMinPoint[kv.Key],
-                        rgba = Rgba32ToUInt32(islandColorByMinPoint[kv.Key]),
-                        maxRect = maxRectByMinPoint[p],
-                    };
-                }
-
-
-                var outputPath = Path.Combine(outputDir, stageName + ".bytes");
-                Directory.CreateDirectory(Path.GetDirectoryName(outputDir));
-                using (var stream = File.Create(outputPath)) {
-                    var formatter = new BinaryFormatter();
-                    formatter.Serialize(stream, stageData);
-                    stream.Close();
-                }
-
-                Console.WriteLine($"{stageData.islandDataByMinPoint.Count} islands loaded.");
-                Console.WriteLine($"Written to {outputPath}");
-
-
-            }
-        }
-
-        // 최대 maxColor개의 색으로 픽셀을 구분하도록 한다.
-        // 즉, 이미지가 팔레트화(indexed color)된다.
-        private static string Quantize(string sourcePngFileName, string stageName, string outputDir, int maxColor) {
-            var quantizer = new WuQuantizer(null, maxColor);
-            Configuration config = Configuration.Default;
-            //using (var result = quantizer.CreateFrameQuantizer<Rgba32>(config).QuantizeFrame(image.Frames[0])) {
-            //}
-
-            using (var image = Image.Load<Rgba32>(sourcePngFileName))
-            using (var result = quantizer.CreateFrameQuantizer<Rgba32>(config).QuantizeFrame(image.Frames[0])) {
-                //for (int h = 0; h < image.Height; h++) {
-                //    for (int w = 0; w < image.Width; w++) {
-                //        var pixelColor = image[w, h];
-                //        if (pixelColor != Rgba32.Black) {
-                //            image[w, h] = Rgba32.White;
-                //        }
-                //    }
-                //}
-
-                List<Color> quantizedPalette = new List<Color>();
-                var blackIncluded = false;
-                foreach (var p in result.Palette.Span) {
-                    var c = new Color(p);
-                    quantizedPalette.Add(c);
-                    if (c == Color.Black) {
-                        blackIncluded = true;
-                    }
-                }
-
-                // 팔레트에 검은색은 무조건 추가되도록 한다.
-                if (blackIncluded == false) {
-                    quantizedPalette.Add(Color.Black);
-                }
-
-                var outputPath = Path.Combine(outputDir, stageName + "-Quantized.png");
-                using (var stream = new FileStream(outputPath, FileMode.Create)) {
-                    var encoder = new SixLabors.ImageSharp.Formats.Png.PngEncoder {
-                        Quantizer = new PaletteQuantizer(quantizedPalette.ToArray(), false), // new WuQuantizer(null, maxColor),
-                        ColorType = SixLabors.ImageSharp.Formats.Png.PngColorType.Palette,
-                    };
-                    image.SaveAsPng(stream, encoder);
-                    stream.Close();
-                }
-
-                return outputPath;
-            }
-        }
-
-        private static void WriteOutlineImageFile(string sourcePngFileName, string stageName, string outputDir) {
-            using (var image = Image.Load<Rgba32>(sourcePngFileName)) {
-                for (int h = 0; h < image.Height; h++) {
-                    for (int w = 0; w < image.Width; w++) {
-                        var pixelColor = image[w, h];
-                        if (pixelColor != Rgba32.Black) {
-                            image[w, h] = Rgba32.White;
-                        }
-                    }
-                }
-
-                var outputPath = Path.Combine(outputDir, stageName + "-Outline.png");
-                using (var stream = new FileStream(outputPath, FileMode.Create)) {
-                    image.SaveAsPng(stream);
-                    stream.Close();
-                }
-            }
         }
 
         private static ulong GetRectRange(int v1, int v2, int v3, int v4) {
