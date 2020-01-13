df6d53db57914ed2efbe7090990c53466ffe9539
Geoyeob Kim <gasbank@gmail.com>
2020-01-13 22:30:38 +0900

    에디터 전용 코드 빌드 시 제외

--- Assets/FrameThickness.cs
+++ Assets/FrameThickness.cs
@@ -13,12 +13,16 @@ public class FrameThickness : MonoBehaviour
     [SerializeField] RectTransform m20 = null;
     [SerializeField] RectTransform m21 = null;
     [SerializeField] RectTransform m22 = null;
-    
+
+#if UNITY_EDITOR    
     void OnValidate() {
-        UnityEditor.EditorApplication.delayCall += () => {
-            m00.sizeDelta = m02.sizeDelta = m20.sizeDelta = m22.sizeDelta = Vector2.one * thickness;
-            m01.sizeDelta = m21.sizeDelta = new Vector2(m01.sizeDelta.x, thickness);
-            m10.sizeDelta = m12.sizeDelta = new Vector2(thickness, m10.sizeDelta.y);
-        };
+        if (Application.isPlaying == false) {
+            UnityEditor.EditorApplication.delayCall += () => {
+                m00.sizeDelta = m02.sizeDelta = m20.sizeDelta = m22.sizeDelta = Vector2.one * thickness;
+                m01.sizeDelta = m21.sizeDelta = new Vector2(m01.sizeDelta.x, thickness);
+                m10.sizeDelta = m12.sizeDelta = new Vector2(thickness, m10.sizeDelta.y);
+            };
+        }
     }
+#endif
 }
