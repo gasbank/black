1effd77f5afa1d3188fb424121b2b2e49f5f8638
Geoyeob Kim <gasbank@gmail.com>
2020-01-15 01:18:48 +0900

    컬러링 시작과 끝 연출 작업 중

---
+++ Assets/MainCanvasDirector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MainCanvasDirector : MonoBehaviour {
+    [SerializeField] CanvasGroup debugGroup = null;
+    [SerializeField] CanvasGroup nameplateGroup = null;
+    [SerializeField] CanvasGroup coinGroup = null;
+    [SerializeField] CanvasGroup paletteGroup = null;
+    
+    enum Mode {
+        Entering,
+        Painting,
+        Finishing,
+        Finished,
+    }
+    [SerializeField] Mode mode = Mode.Entering;
+
+    void OnValidate() {
+        switch (mode) {
+            case Mode.Entering:
+            break;
+            case Mode.Finished:
+            break;
+            case Mode.Finishing:
+            break;
+            case Mode.Painting:
+            break;
+        }
+    }
+}
