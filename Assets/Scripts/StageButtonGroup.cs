b6df4cc824e0c54091d84a13735dcae213fdb2f0
Geoyeob Kim <gasbank@gmail.com>
2021-12-07 21:03:25 +0900

    Addressables 패키지 추가 및 초기 셋업

---
+++ Assets/Scripts/StageButtonGroup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class StageButtonGroup : MonoBehaviour
+{
+    [SerializeField]
+    GameObject stageButtonPrefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
