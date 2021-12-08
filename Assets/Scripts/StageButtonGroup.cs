fdfe448ad76f27f758a9020bcc304302200042e0
Geoyeob Kim <gasbank@gmail.com>
2021-12-08 17:19:46 +0900

    9. 스테이지 선택 화면 #15 - 스테이지 상세 정보 단계 WIP

--- Assets/Scripts/StageButtonGroup.cs
+++ Assets/Scripts/StageButtonGroup.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -8,12 +9,23 @@ public class StageButtonGroup : MonoBehaviour
     
     async void Start()
     {
-        var stageAssetList = await Addressables.LoadAssetsAsync<StageMetadata>("Stage", null).Task;
-        foreach (var stageMetadata in stageAssetList)
+        var stageAssetLocList = await Addressables.LoadResourceLocationsAsync("Stage", typeof(StageMetadata)).Task;
+        foreach (var loc in stageAssetLocList)
         {
+            var stageMetadata = await Addressables.LoadAssetAsync<StageMetadata>(loc).Task;
             var stageButton = Instantiate(stageButtonPrefab, transform).GetComponent<StageButton>();
             stageButton.SetStageMetadata(stageMetadata);
             stageButton.gameObject.name = stageMetadata.name;
         }
+        
+        
+//        var stageAssetList = await Addressables.LoadAssetsAsync<StageMetadata>("Stage", null).Task;
+//        foreach (var stageMetadata in stageAssetList)
+//        {
+//            var stageButton = Instantiate(stageButtonPrefab, transform).GetComponent<StageButton>();
+//            stageButton.SetStageMetadata(stageMetadata);
+//            stageButton.gameObject.name = stageMetadata.name;
+//            await Task.Delay(1);
+//        }
     }
 }
