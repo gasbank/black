7cd5ded30e0ffba9c2b5e261aa9e4c8054310399
Geoyeob Kim <gasbank@gmail.com>
2021-12-10 18:52:22 +0900

    다시 전설의 시작

--- Assets/Scripts/Extension/CanvasGroupExtension.cs
+++ Assets/Scripts/Extension/CanvasGroupExtension.cs
@@ -4,6 +4,8 @@ public static class CanvasGroupExtension
 {
     public static void Show(this CanvasGroup cg)
     {
+        if (cg == null) return;
+        
         cg.alpha = 1.0f;
         cg.interactable = true;
         cg.blocksRaycasts = true;
@@ -11,6 +13,8 @@ public static class CanvasGroupExtension
 
     public static void Hide(this CanvasGroup cg)
     {
+        if (cg == null) return;
+        
         cg.alpha = 0.0f;
         cg.interactable = false;
         cg.blocksRaycasts = false;
