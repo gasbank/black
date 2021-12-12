192836af932df13e825930fc8a4dac1c1eb886c6
Geoyeob Kim <gasbank@gmail.com>
2021-12-12 17:39:21 +0900

    Play Games 설정

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -36,7 +36,7 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = "674192322367"; // Filled in automatically
+        public const string ApplicationId = "1085200398378"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
         public const string WebClientId = ""; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
