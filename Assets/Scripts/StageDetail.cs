2ea9c1d0bafd960161d44f07cf5948031cfa9f96
Geoyeob Kim <gasbank@gmail.com>
2021-12-14 16:08:58 +0900

    스테이지 입장 절차

--- Assets/Scripts/StageDetail.cs
+++ Assets/Scripts/StageDetail.cs
@@ -1,8 +1,18 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class StageDetail : MonoBehaviour
 {
+    [SerializeField]
+    Subcanvas subcanvas;
+    
     public void OpenPopup()
+    {
+        subcanvas.Open();
+    }
+
+    [UsedImplicitly]
+    void ClosePopup()
     {
     }
 }
