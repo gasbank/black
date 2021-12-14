a744c3f9c52390abd79e18c71cf4fcbf296e6433
Geoyeob Kim <gasbank@gmail.com>
2021-12-14 15:05:15 +0900

    iOS App Store 빌드 스크립트

---
+++ build-ios-appstore.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+UNITY_VERSION=`cat ProjectSettings/ProjectVersion.txt | head -n1 | awk '{print $2;}'`
+
+if [ -z "${BUILD_NUMBER}" ]; then
+  BUILD_NUMBER="<NO ENV>"
+fi
+
+/Applications/Unity/Hub/Editor/${UNITY_VERSION}/Unity.app/Contents/MacOS/Unity \
+    -quit \
+    -batchmode \
+    -executeMethod BlackBuild.PerformIosAppStoreBuild \
+    -logfile build.log \
+    -projectPath `pwd` \
+    -buildTarget iOS \
+    -buildNumber ${BUILD_NUMBER} \
+    -noGraphics \
+    -username ${UNITY_USERNAME} \
+    -password ${UNITY_PASSWORD}
