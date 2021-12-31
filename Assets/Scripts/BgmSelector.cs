5e0630dee3157d727f7f069c090d0df647c592b1
Geoyeob Kim <gasbank@gmail.com>
2021-12-31 20:09:13 +0900

    배경음악 연결

---
+++ Assets/Scripts/BgmSelector.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class BgmSelector : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource bgmSource;
+    
+    [SerializeField]
+    AudioClip lobbyBgm;
+
+    [SerializeField]
+    AudioClip[] mainBgm;
+    
+#if UNITY_EDITOR
+    void OnValidate()
+    {
+        AutoBindUtil.BindAll(this);
+    }
+#endif
+
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "Stage Selection")
+        {
+            bgmSource.clip = lobbyBgm;
+        }
+        else
+        {
+            var stageIndex = GetCurrentStageIndex();
+            
+            if (mainBgm.Length > 0 && stageIndex >= 0)
+            {
+                bgmSource.clip = mainBgm[stageIndex % mainBgm.Length];
+            }
+        }
+
+        bgmSource.Play();
+    }
+
+    static int GetCurrentStageIndex()
+    {
+        if (StageButton.CurrentStageMetadata == null
+            || Data.dataSet == null
+            || Data.dataSet.StageSequenceData == null)
+        {
+            return -1;
+        }
+        
+        var stageName = StageButton.CurrentStageMetadata.name;
+        for (var i = 0; i < Data.dataSet.StageSequenceData.Count; i++)
+        {
+            if (Data.dataSet.StageSequenceData[i] != null
+                && Data.dataSet.StageSequenceData[i].stageName == stageName)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
