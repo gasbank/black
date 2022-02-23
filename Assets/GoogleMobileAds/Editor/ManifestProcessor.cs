41c451f00572c7c0b72db92afcd8b955f13d2e95
Geoyeob Kim <gasbank@gmail.com>
2022-02-23 15:37:20 +0900

    뭐이런... https://stackoverflow.com/questions/57920250/androidmanifest-xml-is-missing-try-reimporting-the-plugin

--- Assets/GoogleMobileAds/Editor/ManifestProcessor.cs
+++ Assets/GoogleMobileAds/Editor/ManifestProcessor.cs
@@ -35,7 +35,7 @@ public class ManifestProcessor : IPreprocessBuild
 {
     private const string META_APPLICATION_ID  = "com.google.android.gms.ads.APPLICATION_ID";
 
-    private const string MANIFEST_RELATIVE_PATH = "Plugins/Android/GoogleMobileAdsPlugin.androidlib/AndroidManifest.xml";
+    private const string MANIFEST_RELATIVE_PATH = "Plugins/Android/GoogleMobileAdsPlugin/AndroidManifest.xml";
 
     private const string META_DELAY_APP_MEASUREMENT_INIT =
             "com.google.android.gms.ads.DELAY_APP_MEASUREMENT_INIT";
