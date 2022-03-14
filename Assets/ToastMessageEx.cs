f5a07e7e4a614bada0bb506a905b8ae7da9eb55a
driver <driver@drive.com>
2022-03-15 03:04:11 +0900

    신규 토스트 메시지 작업 시작

---
+++ Assets/ToastMessageEx.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToastMessageEx : MonoBehaviour
+{
+    [SerializeField]
+    public Image background;
+
+    [SerializeField]
+    public Text message;
+
+    [SerializeField]
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
