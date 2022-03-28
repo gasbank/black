53ca3a9c81339fe0f0998047c3c5d5327abb7337
jaeyoung.choi <lactrious@gmail.com>
2022-03-28 13:30:43 +0900

    [62] Say good-bye to BE

--- be/transform/StageData.cs
+++
@@ -1,7 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class StageData {
-    public Dictionary<uint, IslandData> islandDataByMinPoint = new Dictionary<uint, IslandData>();
-}
