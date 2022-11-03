9396d1438695c5337f0fc3bfc917b596299df1d8
Geoyeob Kim <gasbank@msn.com>
2022-11-03 22:59:37 +0900

    BlackBuild.PerformIosBuild()

--- Assets/Scripts/Editor/BlackBuild.cs
+++ Assets/Scripts/Editor/BlackBuild.cs
@@ -9,6 +9,7 @@ using JetBrains.Annotations;
 using UnityEditor.AddressableAssets.Settings;
 #endif
 using UnityEditor.Build.Reporting;
+using UnityEditor.CrashReporting;
 
 internal static class BlackBuild {
     static List<string> ListAllScenes()
@@ -161,95 +162,46 @@ internal static class BlackBuild {
     }
 
     [UsedImplicitly]
-    public static void PerformIosAdHocBuild()
-    {
-        PerformIosDistributionBuild(Environment.GetEnvironmentVariable("IOS_TEAM_ID"),
-            Environment.GetEnvironmentVariable("IOS_AD_HOC_PROFILE_ID"), false);
+    public static void PerformIosBuild() {
+        PerformIosDistributionBuild(Environment.GetEnvironmentVariable("IOS_PROFILE_GUID"));
     }
 
-    [UsedImplicitly]
-    public static void PerformIosAppStoreBuild()
-    {
-        PerformIosDistributionBuild(Environment.GetEnvironmentVariable("IOS_TEAM_ID"),
-            Environment.GetEnvironmentVariable("IOS_APP_STORE_PROFILE_ID"), true);
-    }
-
-    static void PerformIosDistributionBuild(string teamId, string profileId, bool universal)
-    {
-#if ADDRESSABLES
-        AddressableAssetSettings.BuildPlayerContent();
-#endif
-        if (Application.isBatchMode && string.IsNullOrEmpty(teamId))
-        {
-            Debug.LogError($"Team ID: {teamId} is empty.");
-            EditorApplication.Exit(-1);
-        }
-
-        BuildPlayerOptions options = new BuildPlayerOptions
-        {
+    static void PerformIosDistributionBuild(string profileId) {
+        BuildPlayerOptions options = new BuildPlayerOptions {
             scenes = Scenes, target = BuildTarget.iOS, locationPathName = "./build"
         };
         // Pro 버전일때만 되는 기능이긴 한데, 이걸 켜고 푸시한 경우도 있을테니 여기서 꺼서 안전장치로 작동하게 한다.
         PlayerSettings.SplashScreen.show = false;
-        // DEV_BUILD 심볼을 빼서 디버그 메시지 나오지 않도록 한다.
-        if (Environment.GetEnvironmentVariable("DEV_BUILD") != "1")
-        {
+        // 디버그 관련 심볼을 빼서 디버그 메시지 나오지 않도록 한다.
+        if (Environment.GetEnvironmentVariable("DEV_BUILD") != "1") {
             RemovingBlackDebugDefine(BuildTargetGroup.iOS);
+            
+            // Unity Cloud Diagnostics 활성화한다.
+            // 실서비스 빌드는 리포트 켜서 활용한다.
+            CrashReportingSettings.enabled = true;
+        } else {
+            // 개발중 버전 크래시는 리포트 굳이 받을 필요 없다.
+            CrashReportingSettings.enabled = false;   
         }
+        
+        Debug.Log($"Setting CrashReportingSettings.enabled to '{CrashReportingSettings.enabled}'");
 
-        PlayerSettings.iOS.appleDeveloperTeamID = teamId;
-        if (string.IsNullOrEmpty(profileId))
-        {
+        PlayerSettings.iOS.appleDeveloperTeamID = Environment.GetEnvironmentVariable("IOS_TEAM_ID");
+        if (string.IsNullOrEmpty(profileId)) {
             PlayerSettings.iOS.appleEnableAutomaticSigning = true;
-        }
-        else
-        {
+        } else {
             PlayerSettings.iOS.appleEnableAutomaticSigning = false;
             PlayerSettings.iOS.iOSManualProvisioningProfileID = profileId;
             PlayerSettings.iOS.iOSManualProvisioningProfileType = ProvisioningProfileType.Distribution;
         }
 
-        PlayerSettings.SetArchitecture(BuildTargetGroup.iOS,
-            (int) (universal ? AppleMobileArchitecture.Universal : AppleMobileArchitecture.ARM64));
-
         var cmdArgs = Environment.GetCommandLineArgs().ToList();
         ProcessBuildNumber(cmdArgs);
-        UnbindAllTemporaryAssets();
         var buildReport = BuildPipeline.BuildPlayer(options);
-        if (buildReport.summary.result != BuildResult.Succeeded && Application.isBatchMode)
-        {
+        if (buildReport.summary.result != BuildResult.Succeeded && Application.isBatchMode) {
             EditorApplication.Exit(-1);
         }
     }
-
-    // 에디터 환경에서 개발 중 임시로 연결했던 애셋의 연결을 끊는다.
-    static void UnbindAllTemporaryAssets()
-    {
-        // 에디터 열어놓고 하는 빌드에서는 이 작업 하면 안된다.
-        if (Application.isBatchMode == false) return;
-        
-//        var dialogScene = EditorSceneManager.OpenScene("Assets/Scenes/Dialog.unity");
-//        
-//        UnbindAllTemporaryAssetsFromScene<BackgroundImageSelector>(dialogScene);
-//        UnbindAllTemporaryAssetsFromScene<StoreBackgroundImageSelector>(dialogScene);
-//        UnbindAllTemporaryAssetsFromScene<EmojiImageSelector>(dialogScene);
-//        UnbindAllTemporaryAssetsFromScene<PortraitImageSelector>(dialogScene);
-//        UnbindAllTemporaryAssetsFromScene<ImageSpriteUnbinder>(dialogScene);
-//        
-//        EditorSceneManager.SaveScene(dialogScene);
-//        EditorSceneManager.CloseScene(dialogScene, true);
-    }
-
-//    static void UnbindAllTemporaryAssetsFromScene<T>(Scene scene) where T : IResourceSelector
-//    {
-//        var selectorList = scene.GetRootGameObjects()
-//            .Select(e => e.GetComponentsInChildren<T>())
-//            .SelectMany(e => e);
-//        foreach (var s in selectorList)
-//        {
-//            s.Unbind();
-//        }
-//    }
 
     static void RemovingBlackDebugDefine(BuildTargetGroup buildTargetGroup)
     {
